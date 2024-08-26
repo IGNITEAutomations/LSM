1d7c95e4cc23030f40999e1b21c6bf9f6c0f5543
ignitecopilot <CTO@ignitecopilot.ai>
2024-08-26 10:46:37 +0200

    last

--- modules/user/domain/entity/user.ts
+++
@@ -1,5 +0,0 @@
-import {UserRoles} from "@/modules/user/domain/user-roles";
-
-export default class User {
-    constructor(readonly uid: string, readonly token: string, readonly  displayName: string, readonly name: string, readonly email: string, readonly role: UserRoles){}
-}
