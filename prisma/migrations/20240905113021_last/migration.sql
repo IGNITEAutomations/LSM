984d5d579ed3abfe7b7145c4ccebfb20ecc5d107
IGNITEAutomations <ignite.automations@gmail.com>
2024-10-01 22:50:18 +0200

    Delete prisma/migrations/20240905113021_last directory

--- prisma/migrations/20240905113021_last/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "Class" ALTER COLUMN "id" DROP DEFAULT;
-DROP SEQUENCE "Class_id_seq";
-
--- AlterTable
-ALTER TABLE "School" ALTER COLUMN "id" DROP DEFAULT;
-DROP SEQUENCE "School_id_seq";
