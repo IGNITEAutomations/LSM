fc6a2565690cb7ae4b7c5be2f83853e41e0cb579
IGNITEAutomations <ignite.automations@gmail.com>
2025-02-21 12:42:24 +0100

    Cambiar diseño del botón challenge

--- app/(logged)/(teacher)/challenges/table.tsx
+++ app/(logged)/(teacher)/challenges/table.tsx
@@ -1,29 +1,37 @@
-import {ChallengeDataRow} from "@/hooks/ChallengesProvider";
-import {Table, TBody, TCell, THead, TRow} from "@/app/(logged)/_components/Table";
+import { ChallengeDataRow } from "@/hooks/ChallengesProvider";
+import { Table, TBody, TCell, THead, TRow } from "@/app/(logged)/_components/Table";
 
 type ChallengesTableProps = {
-    matrix: ChallengeDataRow[];
-    headers: string[];
-    onChange: (row: number, col: number, value: boolean) => void;
+  matrix: ChallengeDataRow[];
+  headers: string[];
+  onChange: (row: number, col: number, value: boolean) => void;
 };
 
-export default function ChallengesTable({matrix, headers, onChange}: ChallengesTableProps) {
-    return (<Table>
-            <THead headers={headers}/>
-            <TBody>
-                {matrix?.map((row, rowIndex) => (<TRow key={rowIndex}>
-                        <TCell>
-                            <p className="w-full text-left pl-2">{row.student.displayName}</p>
-                        </TCell>
-                        {row.challenges.map((challenge, colIndex) => (<TCell key={colIndex}>
-                                <input
-                                    type="checkbox"
-                                    className="h-4 w-4"
-                                    checked={challenge.value as boolean}
-                                    onChange={(event) => onChange(rowIndex, colIndex, event.currentTarget.checked)}
-                                />
-                            </TCell>))}
-                    </TRow>))}
-            </TBody>
-        </Table>);
+export default function ChallengesTable({ matrix, headers, onChange }: ChallengesTableProps) {
+  return (
+    <Table>
+      <THead headers={headers} />
+      <TBody>
+        {matrix?.map((row, rowIndex) => (
+          <TRow key={rowIndex}>
+            <TCell>
+              <p className="w-full text-left pl-2">{row.student.displayName}</p>
+            </TCell>
+            {row.challenges.map((challenge, colIndex) => (
+              <TCell key={colIndex}>
+                <div
+                  onClick={() => onChange(rowIndex, colIndex, !challenge.value)}
+                  className={`h-6 w-6 flex items-center justify-center cursor-pointer ${
+                    challenge.value ? "text-red-500" : "text-green-500"
+                  }`}
+                >
+                  {challenge.value ? "❌" : "✔️"}
+                </div>
+              </TCell>
+            ))}
+          </TRow>
+        ))}
+      </TBody>
+    </Table>
+  );
 }
